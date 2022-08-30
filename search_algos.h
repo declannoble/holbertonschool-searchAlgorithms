fae88f40729b83f03204e164e940d94cf9b32377
declannoble <declanjnoble@hotmail.com>
2022-08-30 10:55:43 +1000

    build: add library

---
+++ search_algos.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif 
